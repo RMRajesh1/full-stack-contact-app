0e6200caad0e0eadfccc04c78f57d7fd510559b9
Rajesh M <rajes-zsch951@rajes-zsch951.csez.zohocorpin.com>
2021-05-18 08:06:01 +0530

    updated files

--- sql/database.sql
+++ sql/database.sql
@@ -1,4 +1,3 @@
-
 DROP DATABASE IF EXISTS contact_application;
 
 CREATE DATABASE contact_application;
@@ -11,7 +10,13 @@ CREATE TABLE number (number_id VARCHAR(20) NOT NULL, number BIGINT NOT NULL, typ
 DROP TABLE contact;
 DROP TABLE number;
 
+INSERT INTO contact VALUES ("kglo4jmd", "/contact-app/home/assets/images/avatar-370-456322-512.webp", "Lady", "my-avatar@mail.com", "Describe!", 1621298657973);
+INSERT INTO number VALUES ("q75pvk6c", 0987654321, 0, "kglo4jmd");
+INSERT INTO number VALUES ("a2o6kil0", 1234567890, 1, "kglo4jmd");
 
+INSERT INTO contact VALUES ("q2kftmhs", "/contact-app/home/assets/images/woman-sporty-brunette.webp", "Lady", "lady@mail.com", "Describe about lady!", 1621299079796);
+INSERT INTO number VALUES ("v7kp9h9v", 6547389201, 2, "q2kftmhs");
+INSERT INTO number VALUES ("47ks04ah", 0129384756, 3, "q2kftmhs");
 
 
 
