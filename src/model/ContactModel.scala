d9f96896fa671e0391046f64ce136c27465797f5
Rajesh M <rajes-zsch951@rajes-zsch951.csez.zohocorpin.com>
2021-05-21 11:43:44 +0530

    Add - This file contains the Model class for Contact and Number

--- src/model/ContactModel.scala
+++ src/model/ContactModel.scala
@@ -1,34 +1,10 @@
 package model
 
-import scala.collection.mutable.{ListBuffer, Map}
-import play.api.libs.json._
-import workflow._
-
 class ContactModel {
-
-  def getContacts(): ListBuffer[Map[String, String]] = {
-    val workflow = new ContactWorkflow()
-    workflow.getAllContacts()
-  }
-
-  def getContact(id:Int): ListBuffer[Map[String, String]] = {
-    val workflow = new ContactWorkflow()
-    workflow.getContact(id)
-  }
-
-  def updateContact(id:Int, updatedContact:JsValue): Map[String, String] = {
-    val workflow = new ContactWorkflow()
-    workflow.updateContact(id, updatedContact)
-  }
-
-  def createContact(contact:JsValue): Map[String, String] = {
-    val workflow = new ContactWorkflow()
-    workflow.createContact(contact)
-  }
-
-  def deleteContact(id:Int): Map[String, String] = {
-    val workflow = new ContactWorkflow()
-    workflow.deleteContact(id)
-  }
-
+  var id = ""
+  var name = ""
+  var image = ""
+  var email = ""
+  var description = ""
+  var date = ""
 }
